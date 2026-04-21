ce136bfa168f985f71a8c3eed3136dc48e39e544
marmottatonante <tommaso.detommaso@protonmail.com>
2026-04-21 17:35:41 +0200

    Changed name and navbar now reacts to current page.

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
 <div class="flex flex-col md:flex-row justify-between items-center w-full px-12 py-10 border-t border-neutral-800 max-w-screen-2xl mx-auto">
 <div class="mb-8 md:mb-0">
 <div class="font-['EB_Garamond'] text-lg font-semibold mb-2 text-white">Alma Mater Studiorum</div>
-<div class="font-['Epilogue'] text-xs font-light tracking-wide uppercase text-neutral-100">Università di Bologna • Cineforum Project</div>
+<div class="font-['Epilogue'] text-xs font-light tracking-wide uppercase text-neutral-100">Università di Bologna • Cineforum di Ateneo</div>
 </div>
 <div class="flex gap-12 mb-8 md:mb-0">
 <a class="font-['Epilogue'] text-xs font-light tracking-wide hover:text-neutral-200 transition-all uppercase text-neutral-100 hover:text-white" href="#">Privacy Policy</a>
