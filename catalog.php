5a24e169330e2a79a68fa0747c2f438286f8deb8
marmottatonante <tommaso.detommaso@protonmail.com>
2026-04-23 17:44:55 +0200

    Catalog now works

--- catalog.php
+++ catalog.php
@@ -79,7 +79,9 @@
             <div class="flex-grow">
                 <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-8 gap-y-16">
                     <?php
-                        $movies = getMoviesCatalog(1, 6, null, null) ?? [];
+                        $requested_genre = $_GET["genreId"] ?? null;
+                        $requested_director = $_GET["directorName"] ?? null;
+                        $movies = getMoviesCatalog(1, 6, $requested_genre, $requested_director) ?? [];
                         $count = getMoviesCount()["movie_count"] ?? 0;
                         foreach ($movies as $movie):
                             $card_image = $movie["poster_url"];
