9b3f8e327580266aec70acaaf6c8bae1e4466382
marmottatonante <tommaso.detommaso@protonmail.com>
2026-04-23 15:47:45 +0200

    Fixed identation

--- templates/footer.php
+++ templates/footer.php
@@ -1,18 +1,18 @@
 <footer class="bg-neutral-950">
-<div class="flex flex-col md:flex-row justify-between items-center w-full px-12 py-10 border-t border-neutral-800 max-w-screen-2xl mx-auto">
-<div class="mb-8 md:mb-0">
-<div class="font-['EB_Garamond'] text-lg font-semibold mb-2 text-white">Alma Mater Studiorum</div>
-<div class="font-['Epilogue'] text-xs font-light tracking-wide uppercase text-neutral-100">Università di Bologna • Cineforum di Ateneo</div>
-</div>
-<div class="flex gap-12 mb-8 md:mb-0">
-<a class="font-['Epilogue'] text-xs font-light tracking-wide hover:text-neutral-200 transition-all uppercase text-neutral-100 hover:text-white" href="#">Privacy Policy</a>
-<a class="font-['Epilogue'] text-xs font-light tracking-wide hover:text-neutral-200 transition-all uppercase text-neutral-100 hover:text-white" href="#">Accessibility</a>
-<a class="font-['Epilogue'] text-xs font-light tracking-wide hover:text-neutral-200 transition-all uppercase text-neutral-100 hover:text-white" href="#">Contact Us</a>
-</div>
-<div class="text-right">
-<div class="font-['Epilogue'] text-xs font-light tracking-wide text-neutral-100">
-    © 1088-2024 Alma Mater Studiorum - Università di Bologna. All rights reserved.
-</div>
-</div>
-</div>
+    <div class="flex flex-col md:flex-row justify-between items-center w-full px-12 py-10 border-t border-neutral-800 max-w-screen-2xl mx-auto">
+        <div class="mb-8 md:mb-0">
+            <div class="font-['EB_Garamond'] text-lg font-semibold mb-2 text-white">Alma Mater Studiorum</div>
+            <div class="font-['Epilogue'] text-xs font-light tracking-wide uppercase text-neutral-100">Università di Bologna • Cineforum di Ateneo</div>
+        </div>
+        <div class="flex gap-12 mb-8 md:mb-0">
+            <a class="font-['Epilogue'] text-xs font-light tracking-wide hover:text-neutral-200 transition-all uppercase text-neutral-100 hover:text-white" href="#">Privacy Policy</a>
+            <a class="font-['Epilogue'] text-xs font-light tracking-wide hover:text-neutral-200 transition-all uppercase text-neutral-100 hover:text-white" href="#">Accessibility</a>
+            <a class="font-['Epilogue'] text-xs font-light tracking-wide hover:text-neutral-200 transition-all uppercase text-neutral-100 hover:text-white" href="#">Contact Us</a>
+        </div>
+        <div class="text-right">
+            <div class="font-['Epilogue'] text-xs font-light tracking-wide text-neutral-100">
+                © 1088-2024 Alma Mater Studiorum - Università di Bologna. All rights reserved.
+            </div>
+        </div>
+    </div>
 </footer>
