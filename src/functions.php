63c0547478da89bf14a277978fdd6e2812fb81ff
R3aperCitizen <justr3aper3@gmail.com>
2026-04-23 17:11:51 +0200

    Edited moviescatalog function to get filtered movies

--- src/functions.php
+++ src/functions.php
@@ -28,12 +28,28 @@
         return $results->fetchArray();
     }
 
-    function getMoviesCatalog($page_number, $number_of_movies) {
+    function getMoviesCatalog($page_number, $number_of_movies, $genre_id, $director) {
         global $db;
-        $query = "SELECT * FROM movies, genres WHERE movies.genre_id=genres.genre_id LIMIT ? OFFSET ?;";
-        $stmt = $db->prepare($query);
-        $stmt->bindValue(1, $number_of_movies, SQLITE3_INTEGER);
-        $stmt->bindValue(2, ($page_number * $number_of_movies) - $number_of_movies, SQLITE3_INTEGER);
+        if ($genre_id == 0 || $director == "All") {
+            $query = "SELECT * FROM movies, genres WHERE movies.genre_id=genres.genre_id LIMIT ? OFFSET ?;";
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(1, $number_of_movies, SQLITE3_INTEGER);
+            $stmt->bindValue(2, ($page_number * $number_of_movies) - $number_of_movies, SQLITE3_INTEGER);
+        } elseif($genre_id > 0 && $director == "All") {
+            $query = "SELECT * FROM movies, genres WHERE movies.genre_id=genres.genre_id AND genres.genre_id=? LIMIT ? OFFSET ?;";
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(1, $genre_id, SQLITE3_INTEGER);
+            $stmt->bindValue(2, $number_of_movies, SQLITE3_INTEGER);
+            $stmt->bindValue(3, ($page_number * $number_of_movies) - $number_of_movies, SQLITE3_INTEGER);
+        } elseif($genre_id == 0 && $director != "All") {
+            $query = "SELECT * FROM movies, genres WHERE movies.genre_id=genres.genre_id AND movies.director=? LIMIT ? OFFSET ?;";
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(1, $director, SQLITE3_INTEGER);
+            $stmt->bindValue(2, $number_of_movies, SQLITE3_INTEGER);
+            $stmt->bindValue(3, ($page_number * $number_of_movies) - $number_of_movies, SQLITE3_INTEGER);
+        } else {
+            return null;
+        }
         $results = $stmt->execute();
 
         $movies = [];
@@ -79,6 +95,13 @@
         }
 
         return $movies_directors;
+    }
+
+    function getMoviesCount() {
+        global $db;
+        $query = "SELECT COUNT(*) AS movie_count FROM movies;";
+        $results = $db->query($query);
+        return $results->fetchArray();
     }
 
     function formatDate($dateString) {
