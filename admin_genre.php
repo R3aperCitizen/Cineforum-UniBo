77b4815645c677a46cc75926e231fff18af208a3
marmottatonante <tommaso.detommaso@protonmail.com>
2026-04-25 17:56:18 +0200

    Fixed page titles

--- admin_genre.php
+++ admin_genre.php
@@ -11,7 +11,7 @@
 <html class="" lang="en"><head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>UniBo Cineforum | Admin | Generi</title>
+    <title>UniBo Cineforum | Admin | Genere</title>
     <script src="https://cdn.tailwindcss.com?plugins=forms,container-queries"></script>
     <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500;600;700;800&family=Epilogue:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet">
