96868b9ce2bcd8cce99e3931c43bd5ef4dc42ec9
marmottatonante <tommaso.detommaso@protonmail.com>
2026-04-27 18:50:31 +0200

    Fixed genre_delete if genre connected to movies

--- actions/admin_genre_delete.php
+++ actions/admin_genre_delete.php
@@ -4,6 +4,8 @@
     session_start();
     requireAuthorization();
     $request = requireParams($_POST, ["genre_id"]);
+    if(hasGenreLinkedMovies($request["genre_id"]))
+        throwError(400, "Impossibile rimuovere. Questo genere ha dei film collegati.");
     deleteGenreFromId($request["genre_id"]);
     
     redirect("/admin_genres.php");
